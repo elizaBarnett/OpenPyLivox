e783192af5513eca7c59d774dc1089dda7d04849
Eliza Barnett <35015965+elizaBarnett@users.noreply.github.com>
2025-05-04 20:37:10 -0700

    Update livox_controller_demo.py
    This is so it connects to the lab's Avia

--- livox_controller_demo.py
+++ livox_controller_demo.py
@@ -43,10 +43,10 @@ def singleSensorDemo():
     sensor.resetShowMessages()
 
     # easiest to try to automatically set the openpylivox sensor connection parameters and connect
-    connected = sensor.auto_connect()
+    #connected = sensor.auto_connect()
 
     # or if your computer has multiple IP address you can force the computer IP to a manual address
-    # connected = sensor.auto_connect("192.168.1.23")
+    connected = sensor.auto_connect(manualComputerIP="192.168.1.50") 
 
     # or manually define all IP addresses and ports (still need to properly configure your IP, Subnet, etc. on your computer)
     #                            computer IP       sensorIP    data port  command port  IMU port
